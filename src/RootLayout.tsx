9666bd5efd8ce3e08148a4d8d944142955e6a754
Syna Bansal <SynaBansal2008@gmail.com>
2023-11-26 09:48:16 -0500

    Added react-native-paper

---
+++ src/RootLayout.tsx
@@ -0,0 +1,121 @@
+import React, { useEffect, useState } from "react";
+import {
+  StyleSheet,
+  View,
+  Pressable,
+  SafeAreaView,
+} from "react-native";
+import {Text, TextInput} from 'react-native-paper'
+import { generateClient } from "aws-amplify/api";
+import {
+  withAuthenticator,
+  useAuthenticator,
+} from "@aws-amplify/ui-react-native";
+import { createTodo } from "./graphql/mutations";
+import { listTodos } from "./graphql/queries";
+
+// retrieves only the current value of 'user' from 'useAuthenticator'
+
+const userSelector = (context) => [context.user];
+
+const SignOutButton = () => {
+  const { user, signOut } = useAuthenticator(userSelector);
+
+  return (
+    <Pressable onPress={signOut} style={styles.buttonContainer}>
+      <Text style={styles.buttonText}>
+        Hello, {user.username}! Click here to sign out!
+      </Text>
+    </Pressable>
+  );
+};
+const initialState = { name: "", description: "" };
+const client = generateClient();
+
+export const RootLayout = () => {
+  const [formState, setFormState] = useState(initialState);
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    fetchTodos();
+  }, []);
+
+  function setInput(key, value) {
+    setFormState({ ...formState, [key]: value });
+  }
+
+  async function fetchTodos() {
+    try {
+      const todoData = await client.graphql({
+        query: listTodos,
+      });
+      const todos = todoData.data.listTodos.items;
+      setTodos(todos);
+    } catch (err) {
+      console.log("error fetching todos");
+    }
+  }
+
+  async function addTodo() {
+    try {
+      if (!formState.name || !formState.description) return;
+      const todo = { ...formState };
+      setTodos([...todos, todo]);
+      setFormState(initialState);
+      await client.graphql({
+        query: createTodo,
+        variables: {
+          input: todo,
+        },
+      });
+    } catch (err) {
+      console.log("error creating todo:", err);
+    }
+  }
+
+  return (
+      <View style={styles.container}>
+        {/* <SignOutButton /> */}
+        <TextInput
+          onChangeText={(value) => setInput("name", value)}
+          style={styles.input}
+          value={formState.name}
+          placeholder="Name"
+        />
+        <TextInput
+          onChangeText={(value) => setInput("description", value)}
+          style={styles.input}
+          value={formState.description}
+          placeholder="Description"
+        />
+        <Pressable onPress={addTodo} style={styles.buttonContainer}>
+          <Text style={styles.buttonText}>Create todo</Text>
+        </Pressable>
+        {todos.map((todo, index) => (
+          <View key={todo.id ? todo.id : index} style={styles.todo}>
+            <Text style={styles.todoName}>{todo.name}</Text>
+            <Text style={styles.todoDescription}>{todo.description}</Text>
+          </View>
+        ))}
+      </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: { width: 400, flex: 1, padding: 20, alignSelf: "center" },
+  todo: { marginBottom: 15 },
+  input: {
+    backgroundColor: "#ddd",
+    marginBottom: 10,
+    padding: 8,
+    fontSize: 18,
+  },
+  todoName: { fontSize: 20, fontWeight: "bold" },
+  todoDescription: { fontSize: 20 },
+  buttonContainer: {
+    alignSelf: "center",
+    backgroundColor: "black",
+    paddingHorizontal: 8,
+  },
+  buttonText: { color: "white", padding: 16, fontSize: 18 },
+});
